11427c4e7ec75dc8cbc5c6a67429bef84b810d76
raavinduf <ravinduf@rocketmail.com>
2020-07-01 09:30:22 +0530

    rendered banner in error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 function Error() {
     return (
         <div>
-            <Hero /> 
+            <Hero>
+            
+            <Banner title="404" subtitle="page not found">
+            <Link to='/' className="btn-primary">
+                return home
+            </Link>
+            </Banner>
+            
+          
+            </Hero> 
         </div>
     )
 }
