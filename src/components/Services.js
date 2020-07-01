bc978b31cde1bfe3c53acfb8bf9edfc02ed819ec
raavinduf <ravinduf@rocketmail.com>
2020-07-01 10:17:57 +0530

    set the state for services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
-
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
 import Title from './Title';
 
 class Services extends Component {
+    state = {
+        services:[
+            {
+                icon: <FaCocktail/>,
+                title: "free cocktails",
+                info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam' 
+            },
+            {
+                icon: <FaHiking/>,
+                title: "Endless Hiking",
+                info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam' 
+            },
+            {
+                icon: <FaShuttleVan/>,
+                title: "Free shuttle",
+                info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam' 
+            },
+            {
+                icon: <FaBeer/>,
+                title: "Strongest Beer",
+                info: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quam' 
+            }
+        ]
+    }
+    
     render() {
         return (
-            <div>
+            <section>
                 <Title title="services" />
-                
-            </div>
+                <div className="services-center">
+                    {this.state.services.map(service => (
+                        <ul style={{listStyle: 'none'}}>
+                            <li><h2>{service.icon}</h2></li>
+                            <li><h3>{service.title}</h3></li>
+                            <li>{service.info}</li>
+                        </ul>
+                    ))}
+                </div>
+            </section>
         )
     }
 }
