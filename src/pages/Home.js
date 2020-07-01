d10326f6c48ae5ed600d1c4ca1b607d4b0ff2403
raavinduf <ravinduf@rocketmail.com>
2020-07-01 09:06:48 +0530

    rendered banner in home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 function Home() {
     return (
         <div>
-          <Hero /> 
+          <Hero>
+            <Banner title="luxurious rooms" subtitle="deluxe rooms 
+            starting at $299">
+              <Link to='/rooms' className="btn-primary">
+                our rooms
+              </Link>
+            </Banner>
+            
+          </Hero>
         </div>
     )
 }
