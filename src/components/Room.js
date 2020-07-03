9df0695312489df648edf59b9c13fdacdee20b01
raavinduf <ravinduf@rocketmail.com>
2020-07-03 19:55:19 +0530

    updated Room component

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-function Room() {
+import defaultImg from "../images/room-1.jpeg";
+
+function Room({room}) {
+    const {name, slug, images, price} = room;
     return (
-        <div>
-            Hello from room
-        </div>
+        <article className="room">
+            <div className="img-container">
+                <img src={images[0] || defaultImg } alt="single room"/>
+                <div className="price-top">
+                    <h6>${price}</h6>
+                </div>
+                <Link to={`/rooms/${slug}`} className="btn-primary room-link">Features</Link>
+            </div>
+            <p className="room-info">{name}</p>
+        </article>
     )
+}
+
+Room.propTypes = {
+    room:PropTypes.shape({
+        name:PropTypes.string.isRequired,
+        slug:PropTypes.string.isRequired,
+        price:PropTypes.number.isRequired,
+        images: PropTypes.arrarOf(PropTypes.string).isRequired
+    })
 }
 
 export default Room;
