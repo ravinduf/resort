f13423e1166f5c35749c6348f402e2995ddd4c7e
raavinduf <ravinduf@rocketmail.com>
2020-07-03 15:22:01 +0530

    added format data function

--- src/Context.js
+++ src/Context.js
@@ -11,6 +11,24 @@ export default class RoomProvider extends Component {
         loading: true
     };
 
+    componentDidMount(){
+        let rooms = this.formatData(items);
+    }
+
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id
+            let images = item.fields.images.map(image => image.fields.file.url);
+
+            let room = {...item.fields, images, id};
+
+            return room;
+        });
+
+        return tempItems;
+    }
+
+
     render() {
         return (
             <RoomContext.Provider value={{...this.state}}>
