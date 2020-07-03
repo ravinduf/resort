c4097adabb31eb1e58677e14357b2be7e00ebb96
raavinduf <ravinduf@rocketmail.com>
2020-07-03 12:25:41 +0530

    updated state

--- src/Context.js
+++ src/Context.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import items from './data';
 
 const RoomContext = React.createContext();
 
 export default class RoomProvider extends Component {
+    state = {
+        rooms: [],
+        sortedRooms:[],
+        featuredRooms: [],
+        loading: true
+    };
+
     render() {
         return (
-            <RoomContext.Provider value={"hello"}>
+            <RoomContext.Provider value={{...this.state}}>
                 {this.props.children}
             </RoomContext.Provider>
         )
