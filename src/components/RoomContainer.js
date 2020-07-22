668add3402d5f0d175df0c6c6c4ee3b085e1a111
raavinduf <ravinduf@rocketmail.com>
2020-07-22 16:02:51 +0530

    passed the props to RoomContainer using RoomConsumer
    updated the RoomContainer

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import RoomsFilter from './RoomFilter';
 import RoomsList from './RoomList';
+import {RoomConsumer} from '../Context';
+import Loading from './Loading';
 
 function RoomsContainer() {
+
+    
     return (
-        <div>
-            Hello from Rooms container
-            <RoomsFilter/>
-            <RoomsList/>
-        </div>
+        <RoomConsumer>
+            {
+                (value) => {
+                    const {loading, sortedRooms, rooms} = value;
+                    return (
+                        <div>
+                            Hello from Rooms container
+                            <RoomsFilter/>
+                            <RoomsList/>
+                        </div>
+                    );
+                }
+            }
+        </RoomConsumer>
+        
     )
 }
 
