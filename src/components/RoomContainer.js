49cdeb8ffc387c39c48a738ff4635ff5987c31cb
raavinduf <ravinduf@rocketmail.com>
2020-07-26 01:01:33 +0530

    rendered RoomFilter and RoomList using withRoomsContainer higher order function

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,29 +1,54 @@
 import React from 'react';
 import RoomsFilter from './RoomFilter';
 import RoomsList from './RoomList';
-import {RoomConsumer} from '../Context';
+import { withRoomConsumer } from '../Context';
 import Loading from './Loading';
 
-function RoomsContainer() {
-
-    
+function RoomsContainer({context}) {
+    const {loading, sortedRooms, rooms} = context;
+    if (loading) {
+        return <Loading />;
+    }
     return (
-        <RoomConsumer>
-            {
-                (value) => {
-                    const {loading, sortedRooms, rooms} = value;
-                    return (
-                        <div>
-                            Hello from Rooms container
-                            <RoomsFilter/>
-                            <RoomsList/>
-                        </div>
-                    );
-                }
-            }
-        </RoomConsumer>
-        
-    )
+            <div>
+                Hello from Rooms container
+                <RoomsFilter rooms={rooms} />
+                <RoomsList rooms={sortedRooms} />
+            </div>
+        );
 }
 
-export default RoomsContainer;
+export default withRoomConsumer(RoomsContainer)
+// import React from 'react';
+// import RoomsFilter from './RoomFilter';
+// import RoomsList from './RoomList';
+// import {RoomConsumer} from '../Context';
+// import Loading from './Loading';
+
+// function RoomsContainer() {
+
+    
+//     return (
+//         <RoomConsumer>
+//             {
+//                 (value) => {
+                   
+//                     const {loading, sortedRooms, rooms} = value;
+//                     if (loading) {
+//                         return <Loading />
+//                     }
+//                     return (
+//                         <div>
+//                             Hello from Rooms container
+//                             <RoomsFilter rooms={rooms} />
+//                             <RoomsList rooms={sortedRooms} />
+//                         </div>
+//                     );
+//                 }
+//             }
+//         </RoomConsumer>
+        
+//     )
+// }
+
+// export default RoomsContainer;
