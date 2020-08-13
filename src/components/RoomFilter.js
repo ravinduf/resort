1bb85633acdde07647677e359461f5c1e1f47233
raavinduf <ravinduf@rocketmail.com>
2020-08-13 15:55:10 +0530

    updated RoomFilter with showing room type select

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from '../Context';
+import Title from '../components/Title';
 
-function RoomFilter() {
+// get all unique values
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
+}
+
+function RoomFilter({rooms}) {
+    const context = useContext(RoomContext);
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context;
+    // get unique types
+    let types = getUnique(rooms, 'type');
+    // add all
+    types = ['all', ...types];
+    //map to jsx
+    types = types.map((item, index) => {
+        return (<option value={item} key={index}>{item}</option>);
+    });
+
     return (
-        <div>
-            Hello from Rooms Container
-        </div>
+        <section className="filter-container">
+            <Title title="search rooms"/>
+            <form className = "filter-form"></form>
+            {/* select type */}
+            <div className="form-group">
+                <label htmlFor="type">room type</label>
+                <select name="type" id="type" value={type} className="form-control" onChange={handleChange}>
+                    {types}
+                </select>
+            </div>
+
+            {/* end select type */}
+        </section>
+
     )
 }
 
