cd01f029ad04580e3aa952ed228f6d2e35184551
raavinduf <ravinduf@rocketmail.com>
2020-08-13 21:40:29 +0530

    filtering by capacity added

--- src/Context.js
+++ src/Context.js
@@ -70,13 +70,19 @@ export default class RoomProvider extends Component {
 
     filterRooms = () => {
         let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets} = this.state;
-
+        // all the rooms
         let tempRooms = [...rooms];
+        // transform value
+        capacity = parseInt(capacity);
         
+        //filter by type
         if (type !== 'all') {
             tempRooms = tempRooms.filter(room => room.type === type);
         }
-
+        // filter by capacity
+        if (capacity !== 1) {
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+        }
         this.setState({
             sortedRooms: tempRooms
         });
